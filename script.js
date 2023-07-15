8fa5e2ad3158d49253264c9dc7bb4e697d61bd06
Sina Booeshaghi <sbooeshaghi@gmail.com>
2023-07-15 09:26:39 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,50 @@
+function estimateFileSize() {
+  var numberOfReads = parseInt(document.getElementById("numberOfReads").value);
+  var unitMultiplier = parseInt(
+    document.getElementById("unitMultiplier").value
+  );
+  numberOfReads *= unitMultiplier;
+
+  var basesPerRead = parseInt(document.getElementById("basesPerRead").value);
+
+  var linesPerRead = 4;
+  var readLength = basesPerRead;
+  var qualityScoreLength = basesPerRead;
+  var averageLineLength = readLength + 1 + qualityScoreLength + 1; // Sequence line + newline + Quality score line + newline
+  var compressionRatio = 4; // Estimated average compression ratio
+
+  var estimatedSize =
+    numberOfReads * linesPerRead * averageLineLength * (1 / compressionRatio);
+  var fileSize = formatSize(estimatedSize);
+  var peFileSize = formatSize(estimatedSize * 2);
+
+  var assumptions = {
+    "Number of reads": numberOfReads.toLocaleString(),
+    "Length of read": basesPerRead + "bp",
+    "Length of quality score": basesPerRead + "bp",
+    "Lines per read": "4 (matching the FASTQ file standard)",
+    "Compression ratio": `1 / ${compressionRatio}`,
+  };
+
+  var list = document.getElementById("assumptions");
+  list.textContent = "";
+  for (var key in assumptions) {
+    var listItem = document.createElement("li");
+    listItem.innerHTML = `<b>${key}</b>` + ": " + assumptions[key];
+    list.appendChild(listItem);
+  }
+
+  document.getElementById(
+    "result"
+  ).textContent = `Estimated gzipped FASTQ file size: ${fileSize}, (${peFileSize} for paired-end)`;
+}
+
+function formatSize(sizeInBytes) {
+  var units = ["B", "kB", "MB", "GB", "TB"];
+  var power = 0;
+  while (sizeInBytes >= 1000 && power < units.length - 1) {
+    sizeInBytes /= 1000;
+    power++;
+  }
+  return sizeInBytes.toFixed(2) + " " + units[power];
+}
